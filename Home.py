451864f9e6a47e2208040aa62de2e3879516bc57
Pongsapak <ppongsapak1111@gmail.com>
2023-12-15 14:20:32 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("pongsapak ploymaklam")
+
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('พงศภัค พลอยมะกล่ำ')
+st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
+st.markdown("----")
