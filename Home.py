6c6bb5c0970493627c6e0b8ea4b12c6aefe5a2f5
Pongsapak <ppongsapak1111@gmail.com>
2023-12-15 15:52:07 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
-st.header("pongsapak ploymaklam")
-
-ef load_lottieurl(url: str):
+def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
+
+lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
+lottie3 = load_lottieurl(lot3)
+st_lottie(lottie3)
+
+st.header("pongsapak ploymaklam")
 
 st.title('การทดสอบเขียนเว็บด้วย Python')
 lot3="https://lottie.host/9d7858db-0b59-4395-ae0f-b57203577235/vTJKGelBz0.json"
